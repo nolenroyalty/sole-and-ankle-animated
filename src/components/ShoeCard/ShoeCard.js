a379cf78cc62a954966002cc1d6b4ef4f2da5baf
Nolen Royalty <nolen.royalty@gmail.com>
2024-01-29 13:42:20 -0500

    exercise 1 and goofy text animation

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { keyframes } from "styled-components";
 
-import { WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { WEIGHTS } from "../../constants";
+import { formatPrice, pluralize, isNewShoe } from "../../utils";
+import Spacer from "../Spacer";
 
 const ShoeCard = ({
   slug,
@@ -36,9 +37,19 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
-          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
-          {variant === 'new-release' && (
-            <NewFlag>Just released!</NewFlag>
+          {variant === "on-sale" && (
+            <SaleFlag>
+              <NewFlagText totalTime={0.5} animationPercent={0.8} text="Sale" />
+            </SaleFlag>
+          )}
+          {variant === "new-release" && (
+            <NewFlag>
+              <NewFlagText
+                totalTime={0.9}
+                animationPercent={0.5}
+                text="Just released!"
+              />
+            </NewFlag>
           )}
         </ImageWrapper>
         <Spacer size={12} />
@@ -46,20 +57,18 @@ const ShoeCard = ({
           <Name>{name}</Name>
           <Price
             style={{
-              '--color':
-                variant === 'on-sale'
-                  ? 'var(--color-gray-700)'
-                  : undefined,
-              '--text-decoration':
-                variant === 'on-sale' ? 'line-through' : undefined,
+              "--color":
+                variant === "on-sale" ? "var(--color-gray-700)" : undefined,
+              "--text-decoration":
+                variant === "on-sale" ? "line-through" : undefined,
             }}
           >
             {formatPrice(price)}
           </Price>
         </Row>
         <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-          {variant === 'on-sale' ? (
+          <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" ? (
             <SalePrice>{formatPrice(salePrice)}</SalePrice>
           ) : undefined}
         </Row>
@@ -68,20 +77,88 @@ const ShoeCard = ({
   );
 };
 
+const NewFlagText = ({ text, totalTime = 0.8, animationPercent = 0.4 }) => {
+  const SPEED_FACTOR = 2;
+
+  function animationDelay(position) {
+    return (
+      (position / text.length) * (totalTime - totalTime * animationPercent)
+    );
+  }
+
+  function animationTime(position) {
+    const baseTime = totalTime * animationPercent;
+    const speedDivisor =
+      SPEED_FACTOR - ((SPEED_FACTOR - 0.75) * (position + 1)) / text.length;
+    console.log(
+      `baseTime: ${baseTime}, speedDivisor: ${speedDivisor}, position: ${position}`
+    );
+    return baseTime / speedDivisor + "s";
+  }
+
+  return [...text].map((char, i) => (
+    <FlagChar
+      key={i}
+      style={{
+        "--animation-time": animationTime(i),
+        "--animation-delay": 0.175 + animationDelay(i) + "s",
+      }}
+    >
+      {char}
+    </FlagChar>
+  ));
+};
+
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
+  overflow: hidden;
+  border-radius: 16px 16px 4px 4px;
 `;
 
 const Image = styled.img`
   width: 100%;
-  border-radius: 16px 16px 4px 4px;
+
+  will-change: transform;
+  transition: transform 0.75s ease-in-out;
+  display: block;
+  transform-origin: 50% 80%;
+
+  ${Link}:hover &, ${Link}:focus & {
+    transform: scale(1.1);
+    transition: transform 0.2s ease-in-out;
+  }
+`;
+
+const bumpUpAnimation = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+
+  50% {
+    transform: translateY(-6px);
+  }
+
+  100% {
+    transform: translateY(0px);
+  }`;
+
+const FlagChar = styled.span`
+  will-change: transform, color;
+  display: inline-block;
+  white-space: pre-wrap;
+
+  ${Link}:hover &, ${Link}:focus & {
+    animation: ${bumpUpAnimation} var(--animation-time) ease-in-out forwards;
+    animation-delay: var(--animation-delay);
+  }
 `;
 
 const Row = styled.div`
@@ -121,6 +198,20 @@ const Flag = styled.div`
   font-weight: ${WEIGHTS.bold};
   color: var(--color-white);
   border-radius: 2px;
+`;
+
+const flagAnimation = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+
+  25% {
+    transform: translateY(-4px);
+  }
+
+  100% {
+    transform: translateY(0px);
+  }
 `;
 
 const SaleFlag = styled(Flag)`
